0e17fcb03ae8dee1631f4d8ac7582860bf73a4b5
LaviniaMan <lavi.dude@yahoo.com>
2023-12-11 19:39:28 +0200

    OOP

---
+++ src/Animal.java
@@ -0,0 +1,39 @@
+public class Animal {
+    String nume;
+    String culoare;
+    boolean vegetarian;
+
+    public Animal(){
+        this.nume = "unknown";
+        this.culoare = "unknown";
+        this.vegetarian = false;
+    }
+
+    public Animal(String nume) {
+        this.nume = nume;
+    }
+
+    public Animal(String nume, String cluoare) {
+        this.nume = nume;
+        this.culoare = cluoare;
+    }
+
+    public Animal(String nume, String culoare, boolean vegetarian) {
+        this.nume = nume;
+        this.culoare = culoare;
+        this.vegetarian = vegetarian;
+    }
+
+    public void mananca(){
+        System.out.println(nume+" mananca");
+    }
+
+    public void doarme(){
+        System.out.println(nume+" doarme");
+    }
+
+    public String afiseaza(){
+        return nume+" are culoare"+ culoare;
+    }
+
+}
--- src/Masina.java
+++ src/Masina.java
@@ -1,8 +1,18 @@
 import javax.swing.undo.AbstractUndoableEdit;
 
 public class Masina {
-    String marca = "BMV";
-    int anFabricatie = 2000;
+    String marca;
+    int anFabricatie;
+
+    public Masina() {
+        marca = "BMV";
+        anFabricatie = 2000;
+    }
+
+    public Masina(String marca, int anFabricatie) {
+        this.marca = marca;
+        this.anFabricatie = anFabricatie;
+    }
 
     public int getAnFabricatie() {
         return anFabricatie;
